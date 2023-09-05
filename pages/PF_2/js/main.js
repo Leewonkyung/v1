39827f60f32a3037c27b712383bf248e4718bed6
wonkeeee <wonk0312@naver.com>
2023-09-05 23:17:16 +0900

    main page mark up

---
+++ pages/PF_2/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: "auto",
+    spaceBetween: 48,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+})
