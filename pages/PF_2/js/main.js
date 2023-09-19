3ebe91752f22398efcaf4149270529fb64f9e51e
wonkeeee <wonk0312@naver.com>
2023-09-19 22:38:41 +0900

    add animation

--- pages/PF_2/js/main.js
+++ pages/PF_2/js/main.js
@@ -1,12 +1,39 @@
 $(document).ready(function(){
 
+  $(".visual").addClass("up");
+  setTimeout(function () {
+    
+  });
+
   const swiper = new Swiper('.swiper', {
     slidesPerView: "auto",
     spaceBetween: 48,
     pagination: {
-      el: ".swiper-pagination",
+      el: ".interview .swiper-pagination",
       clickable: true,
     },
   });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.timeline({  
+    scrollTrigger: {
+      trigger: '.category',
+      start: "0% 60%",
+      end: "70% 70%",
+      scrub: 1,
+    },
+    y: -50,
+  })
+  .to('.category .card:nth-of-type(1)', {duration: 0.1, opacity: 1, y: 0})
+  .to('.category .card:nth-of-type(2)', {duration: 0.2, opacity: 1, y: 0})
+  .to('.category .card:nth-of-type(3)', {duration: 0.3, opacity: 1, y: 0})
+
+  ScrollTrigger.create({
+    start: 'top -80',
+    end: 99999,
+    toggleClass: {className: 'fixed', targets: '.header'}
+  });
+  
 
 })
